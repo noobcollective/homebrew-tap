596fa14940d1944c15c495822c01dbc87230911d
iCulture <iculture@mailfence.com>
2024-05-09 14:55:05 +0200

    [CWF] u are kidding me brew?

--- Formula/cwf.rb
+++ Formula/cwf.rb
@@ -2,8 +2,8 @@ class Cwf < Formula
   desc "Copy and paste with friends remotely and easy"
   homepage "https://github.com/DrinoSan/cwf"
   url "https://github.com/DrinoSan/cwf/releases/download/v0.2.3/cwf_0.2.3_darwin_arm64.tar.gz"
-  sha256 "385790bf68fa04992a97aaef3759965b9ce981b7e75b974d428d2fb0887014f7"
   version "0.2.3"
+  sha256 "385790bf68fa04992a97aaef3759965b9ce981b7e75b974d428d2fb0887014f7"
 
   def install
     bin.install "cwf"
