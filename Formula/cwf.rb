cbea6f9c183c740825a157fa542d4ee4ddf264d9
iCulture <iculture@mailfence.com>
2024-05-09 14:21:26 +0200

    [CWF] updated to v0.2.3

--- Formula/cwf.rb
+++ Formula/cwf.rb
@@ -1,8 +1,8 @@
 class Cwf < Formula
   desc "Copy and paste with friends remotely and easy"
   homepage "https://github.com/DrinoSan/cwf"
-  url "https://github.com/DrinoSan/cwf/releases/download/v0.2.2/cwf_0.2.2_darwin_arm64.tar.gz"
-  sha256 "a2c4476f5844c31c2e84df7f8990e04beba8bb517d6c8d8decacc8bad3edb30f"
+  url "https://github.com/DrinoSan/cwf/releases/download/v0.2.3/cwf_0.2.3_darwin_arm64.tar.gz"
+  sha256 "385790bf68fa04992a97aaef3759965b9ce981b7e75b974d428d2fb0887014f7"
 
   def install
     bin.install "cwf"
