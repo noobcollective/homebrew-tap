5a5611e8870df98f7cd4bf3e824bccdf970d785b
iCulture <iculture@mailfence.com>
2024-05-09 14:46:56 +0200

    [CWF] what the actual fuck

--- Formula/cwf.rb
+++ Formula/cwf.rb
@@ -1,9 +1,9 @@
 class Cwf < Formula
   desc "Copy and paste with friends remotely and easy"
   homepage "https://github.com/DrinoSan/cwf"
-  version "0.2.3"
   url "https://github.com/DrinoSan/cwf/releases/download/v0.2.3/cwf_0.2.3_darwin_arm64.tar.gz"
   sha256 "385790bf68fa04992a97aaef3759965b9ce981b7e75b974d428d2fb0887014f7"
+  version "0.2.3"
 
   def install
     bin.install "cwf"
